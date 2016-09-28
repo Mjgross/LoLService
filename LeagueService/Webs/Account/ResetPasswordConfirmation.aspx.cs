eab3479db68d4e048cdba4e053b6ca48d07233d5
Mjgross <mjgross1@asu.edu>
2016-09-27 22:52:57 -0700

    started CSS and html

---
+++ LeagueService/Webs/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Webs.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ LeagueService/Webs/Json/Ranked.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace Webs
+{
+    public class Ranked
+    {
+        [JsonProperty("champions")]
+        public List<ChampionStats> ChampionStats{ get; set;}
+        [JsonProperty("modifyDate")]
+        public long modifyDate { get; set; }
+        [JsonProperty("summonerId")]
+        public long summonerId { get; set; }
+    }
+
+    public class ChampionStats
+    {
+        [JsonProperty("id")]
+        public int id { get; set; }
+        [JsonProperty("stats")]
+        public AggregatedStats aggregatedStats { get; set; }
+    }
+
+    public class Champs
+    {
+        [JsonProperty("maxNumDeaths")]
+        public int MaxNumDeaths { get; set; }
+
+        [JsonProperty("totalDeathsPerSession")]
+        public int TotalDeathsPerSession { get; set; }
+    }
+}
